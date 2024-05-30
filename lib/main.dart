a9f3be1a0e8b2db846f222cb87e0e3adc10e7874
GETTER <122072276+jeremdevzo@users.noreply.github.com>
2024-05-30 01:28:07 -0600

    mi primer commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'dart:io' show Platform;
+
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:provider/provider.dart';
+
+import 'assets.dart';
+import 'title_screen/title_screen.dart';
+
+void main() {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    WidgetsFlutterBinding.ensureInitialized();
+  }
+  Animate.restartOnHotReload = true;
+  runApp(
+    FutureProvider<FragmentPrograms?>(
+      create: (context) => loadFragmentPrograms(),
+      initialData: null,
+      child: const NextGenApp(),
+    ),
+  );
+}
+
+class NextGenApp extends StatelessWidget {
+  const NextGenApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      home: const TitleScreen(),
+    );
+  }
+}
